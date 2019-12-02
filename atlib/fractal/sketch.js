09edf536547024c638d7929f1d4185e6e7468e86
Eli <elipinals12@gmail.com>
2019-12-02 18:08:40 -0500

    atlib V3.0
    added fractal page kinda cool!?1

---
+++ atlib/fractal/sketch.js
@@ -0,0 +1,50 @@
+var h = 180;
+var ang;
+var count = 0;
+
+function setup() {
+    createCanvas(600, 600);
+}
+
+function draw() {
+    background(0);
+    translate(width/2, height);
+
+    stroke(255);
+    strokeWeight(2);
+
+    push();
+    translate(-width/2, -height)
+    ang = radians(map(mouseX, 0, 600, 0, 180));
+    if (ang > PI) {
+        ang = PI;
+    } else if (ang < 0) {
+        ang = 0;
+    }
+    pop();
+
+    branch(176);
+}
+
+function keyPressed() {
+    if (h > 4) {
+        h = h * .67;
+        count++;
+    }
+    print(count);
+}
+
+function branch(len) {
+    line(0, 0, 0, -len);
+    translate(0, -len);
+    if (len > h) {
+        push();
+        rotate(ang);
+        branch(len * .67);
+        pop();
+        push();
+        rotate(-ang);
+        branch(len * .67);
+        pop();
+    }
+}

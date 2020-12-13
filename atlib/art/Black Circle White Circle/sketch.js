1e8b810dc7c5ac6aaf2a7fd7e0e07dfc4ab623c9
Eli <elipinals12@gmail.com>
2020-12-13 00:26:05 -0500

    outline removal
    removed the outline on the circle drawer by default, but dont fret! you can just press space and get it back

--- atlib/art/Black Circle White Circle/sketch.js
+++ atlib/art/Black Circle White Circle/sketch.js
@@ -1,31 +1,46 @@
+var outline = false;
+
 function setup() {
-	createCanvas(window.innerWidth-20, window.innerHeight-20);
+	createCanvas(window.innerWidth - 20, window.innerHeight - 20);
 	//background(random(0, 256), random(0, 256), random(0, 256));
 	setbackground();
-	
+
 	// Get rid of that circle in corner
 	mouseX = -1000
 	mouseY = -1000
 }
 
-function draw() {	
+function draw() {
 	// White vs Black circles
 	if (mouseIsPressed) {
 		fill(0);
-		stroke(255);
+		if (outline) {
+			stroke(255);
+		} else {
+			noStroke();
+		}
+
 	} else {
 		fill(255);
-		stroke(0);
+		if (outline) {
+			stroke(0);
+		} else {
+			noStroke();
+		}
 	}
-	
+
 	// Actually make the circles
 	ellipse(mouseX, mouseY, 80, 80);
-	
+
 	// Clear Screen
 	if (keyIsPressed) {
-		clear();
-		//background(random(0, 256), random(0, 256), random(0, 256));
-		setbackground();
+		if (keyCode == 32) {
+			outline = !outline;
+		} else {
+			clear();
+			//background(random(0, 256), random(0, 256), random(0, 256));
+			setbackground();
+		}
 		keyIsPressed = false;
 	}
 }

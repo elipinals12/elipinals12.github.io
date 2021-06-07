9cad7b6171fa5577cc802d8ced3ea7261dba5ff9
Eli <elipinals12@gmail.com>
2021-06-07 15:45:14 -0400

    quick fix
    made canvas resize on window resize

--- atlib/art/10print/sketch.js
+++ atlib/art/10print/sketch.js
@@ -8,10 +8,10 @@ let prob = 50;
 function setup() {
 
     slider = createSlider(5, 200, 50);
-    slider.position(10,10);
+    slider.position(10, 10);
     slider.style('width', '100px');
-    
-    createCanvas(window.innerWidth-20, window.innerHeight-20);
+
+    createCanvas(window.innerWidth - 20, window.innerHeight - 20);
     background(0);
 }
 
@@ -19,7 +19,7 @@ function draw() {
     if (slider.value() != space) {
         space = slider.value();
         reset();
-        
+
     }
     makelines();
 }
@@ -27,24 +27,29 @@ function draw() {
 
 function reset() {
     background(0);
-    x=0;
-    y=0;
+    x = 0;
+    y = 0;
 }
 
 function makelines() {
-    
+
     color = map(y, 0, height, 255, 0);
     stroke(255, color, color);
 
     if (random(0, 100) > prob) {
-        line(x+space, y, x, y+space);
+        line(x + space, y, x, y + space);
     } else {
-        line(x, y, x+space ,y+space);
+        line(x, y, x + space, y + space);
     }
 
-    x = x+space;
+    x = x + space;
     if (x > width) {
         y = y + space;
         x = 0;
     }
+}
+
+
+function windowResized() {
+    resizeCanvas(window.innerWidth - 20, window.innerHeight - 20);
 }

6db56833132d78ea426bb47b55bbf80eac58a0cb
Eli <elipinals12@gmail.com>
2022-01-10 03:06:59 -0500

    maybe

--- atlib/unlisted/asa/maybe/sketch.js
+++ atlib/unlisted/asa/maybe/sketch.js
@@ -1,28 +1,45 @@
 var yes = true;
+var billcount = 0;
+var billxs = [];
+var billys = [];
+
+let img;
+function preload() {
+	//img = loadImage('burnbill.png');
+}
 
 function setup() {
 	var cnv = createCanvas(windowWidth, windowHeight);
 	var x = (windowWidth - width) / 2;
 	var y = (windowHeight - height) / 2;
 	cnv.position(x, y);
-	background(255);
 }
 
 function draw() {
+	background(255);
 	textAlign(LEFT, TOP);
 	noStroke();
 	fill(0);
 	textSize(12);
 	textFont('Georgia');
 	if (yes) {
-		background(255);
 		text("yes.", 5, 5);
 	} else {
-		background(255);
 		text("no!", 5, 5);
 	}
 	//print(mouseX);
 	//print(mouseY);
+
+	//BILLS RAINING
+	fill(255, 0, 0);
+	for (var i = 0; i < billxs.length; i++) {
+		if (billys[i] > height + 100) {
+			//{ goAwayRedSquare(); }
+		}
+		//rect(billxs[i], billys[i], 30, 30);
+		image(img, billxs[i], billys[i]);
+		billys[i] = billys[i] + 1;
+	}
 }
 
 function mousePressed() {
@@ -30,5 +47,19 @@ function mousePressed() {
 		yes = !yes;
 	} else {
 		print("shit my... pants?")
+	}
+}
+
+function keyPressed() {
+	if (keyIsDown(78) && keyIsDown(70) && keyIsDown(84)) {
+		billcount++;
+		rainMoney();
+	}
+}
+
+function rainMoney() {
+	for (var i = 0; i < pow(billcount, 2); i++) {
+		append(billxs, random(0, width));
+		append(billys, random(-50, -10));
 	}
 }

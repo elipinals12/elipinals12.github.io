8bb06850834b80373f8d20bb28758e786c7684f9
Eli <elipinals12@gmail.com>
2022-01-26 12:19:08 -0500

    coinflip work

--- atlib/tools/coinflip/sketch.js
+++ atlib/tools/coinflip/sketch.js
@@ -1,12 +1,71 @@
-
+let angle = 0;
+let coinsize;
+let flipping = false;
+let counter = 0;
+let outcome;
 
 function setup() {
-    var cnv = createCanvas(600, 600);
-    var x = (windowWidth - width) / 2;
-    var y = ((windowHeight - height) / 2);
-    cnv.position(x, y);
+    var cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+    var cnvx = (windowWidth - width) / 2;
+    var cnvy = (windowHeight - height) / 2;
+    cnv.position(cnvx, cnvy);
+
+    coinsize = width / 10;
+
+    /*textur = createGraphics(30, 30);
+    //texture.background(255, 100);
+    //textur.fill(255);
+    textur.textAlign(CENTER);
+    textur.textSize(20);
+    textur.text('texture', 150, 150);*/
 }
 
 function draw() {
-    background(25);
+    let dx = mouseX - width / 2;
+    let dy = mouseY - height / 2;
+    let v = createVector(dx, dy, 0);
+    v.div(100);
+    background(85);
+    ambientLight(0, 0, 150);
+    //directionalLight(255, 55, 5, v);
+    //directionalLight(55, 155, 5, v2);
+    let locX = mouseX - width / 2;
+    let locY = mouseY - height / 2;
+    pointLight(250, 200, 200, locX, locY, 150);
+    if (flipping) {
+        rotateX(angle * .2);
+        rotateZ(angle * 1);
+        if (counter >= 180) {
+            counter = 0;
+            flipping = false;
+        }
+        counter++;
+    } else {
+        rotateX(angle * .06);
+        rotateZ(angle * .06);
+    }
+    //texture(textur);
+    rotateX(.05);
+    rotateZ(.5);
+    cylinder(coinsize, coinsize / 6, 100, 100);
+    angle += 0.07;
+}
+
+function mousePressed() {
+    flipping = !flipping;
+    if (mouseY > height / 2) {
+        outcome = "r";
+        print("random");
+    } else if (mouseX > width / 2) {
+        outcome = "h";
+        print("heads");
+    } else {
+        outcome = "t";
+        print("tails");
+    }
+}
+
+
+function windowResized() {
+    setup();
 }

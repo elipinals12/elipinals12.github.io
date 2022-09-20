da8da3b52b7041febe42e48b8e2ba93f5d20951a
Eli <elipinals12@gmail.com>
2022-09-19 22:22:41 -0400

    asa crap

---
+++ atlib/unlisted/asa/shoes/sketch.js
@@ -0,0 +1,29 @@
+// let img;
+// function preload() {
+// 	img = loadImage('burnbill.png');
+// }
+
+function setup() {
+	var cnv = createCanvas(windowWidth, windowHeight);
+	var x = (windowWidth - width) / 2;
+	var y = (windowHeight - height) / 2;
+	cnv.position(x, y);
+}
+
+function draw() {
+	background(25, 42, 125);
+
+	
+}
+
+function mousePressed() {
+
+}
+
+function keyPressed() {
+
+}
+
+function windowResized() {
+	setup();
+}

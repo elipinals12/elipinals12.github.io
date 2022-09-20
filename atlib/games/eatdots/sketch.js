85953fde74fed329197603d11d513e4599e60371
Eli <elipinals12@gmail.com>
2022-09-19 22:20:13 -0400

    height fix

--- atlib/games/eatdots/sketch.js
+++ atlib/games/eatdots/sketch.js
@@ -30,7 +30,7 @@ var colorcheat = false;
 function setup() {
     var cnv = createCanvas(windowWidth, windowHeight);
     var cnvx = (windowWidth - width) / 2;
-    var cnvy = (windowHeight - height) / 2;
+    var cnvy = (windowHeight - height) / 2 - 2;
     cnv.position(cnvx, cnvy);
 
     noStroke();

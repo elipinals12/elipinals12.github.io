dc210bd850ad4e143c0b34522a5cd6a36eadd94d
Eli <elipinals12@gmail.com>
2022-12-09 02:28:50 -0500

    sizing

--- atlib/art/ccfinal/sketch.js
+++ atlib/art/ccfinal/sketch.js
@@ -44,7 +44,7 @@ var sc12 = [247, 152, 70];
 //
 
 function setup() {
-    var cnv = createCanvas(windowWidth, windowHeight);
+    var cnv = createCanvas(windowWidth-2, windowHeight-2);
     var x = (windowWidth - width) / 2;
     var y = (windowHeight - height) / 2;
     cnv.position(x, y);

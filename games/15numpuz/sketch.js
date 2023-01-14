e566c07a40921f52e7f09101a9e967b5a880dffe
Eli <elipinals12@gmail.com>
2023-01-14 20:28:35 +0000

    fix

--- games/15numpuz/sketch.js
+++ games/15numpuz/sketch.js
@@ -1,8 +1,8 @@
 var wid, num, blank;
 var pos = [];
-var winstring = "0";
-// var winstring = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0";
-var reversewinstring = "0,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1";
+var winstring = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0";
+// var reversewinstring = "0,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1";
+var reversewinstring = "nope";
 var fader = 255;
 var timefader = 255;
 var winfadeint = 5;

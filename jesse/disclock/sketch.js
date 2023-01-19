2772d7df9e975fb696458628580786c3cbd58dc0
Eli <elipinals12@gmail.com>
2023-01-19 00:46:52 -0500

    jesse 2.0

--- jesse/disclock/sketch.js
+++ jesse/disclock/sketch.js
@@ -4,10 +4,13 @@ let seconds;
 let hourRotation;
 let minuteRotation;
 let secondRotation;
+let minwh;
 
 function setup() {
     windowResized();
     angleMode(DEGREES);
+    
+    minwh = min(width, height);
 }
 
 function draw() {
@@ -23,7 +26,7 @@ function draw() {
     strokeWeight(2);
     stroke(255, 0, 0);
     strokeCap(SQUARE);
-    line(0, 0, 0, -height * 0.4);
+    line(0, 0, 0, -minwh * 0.4);
     fill(0);
     circle(0, 0, 20);
 }
@@ -31,20 +34,22 @@ function draw() {
 function windowResized() {
     var cnv = createCanvas(window.innerWidth - 2, window.innerHeight - 2);
     cnv.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+
+    minwh = min(width, height);
 }
 
 function drawDisks() {
     // Draw hour disk
     fill(255, 100, 150);
-    ellipse(0, 0, height * 0.8, height * 0.8);
+    ellipse(0, 0, minwh * 0.8, minwh * 0.8);
 
     // Draw minute disk
     fill(100, 255, 150);
-    ellipse(0, 0, height * 0.7, height * 0.7);
+    ellipse(0, 0, minwh * 0.7, minwh * 0.7);
 
     // Draw second disk
     fill(150, 100, 255);
-    ellipse(0, 0, height * 0.6, height * 0.6);
+    ellipse(0, 0, minwh * 0.6, minwh * 0.6);
 }
 
 function drawTimeNumbers() {
@@ -54,41 +59,41 @@ function drawTimeNumbers() {
     seconds = now.getSeconds();
 
     hourRotation = map(hours % 24, 0, 24, 0, 360);
-    minuteRotation = map(minutes - 1, 0, 59, 0, 360);
+    minuteRotation = map(minutes-1, 0, 59, 0, 360);
     secondRotation = map(seconds, 0, 60, 0, 360);
 
     fill(0);
     // Hour numbers
-    textSize(height * 0.041);
+    textSize(minwh * 0.041);
     textAlign(CENTER,CENTER);
     for (let i = 0; i <= 23; i++) {
         push();
         rotate(i * (360 / 24) - hourRotation);
-        text(i, 0, -(height * 0.37));
+        text(i, 0, -(minwh * 0.37));
         pop();
     }
 
     // Minute numbers
-    textSize(height * 0.035);
+    textSize(minwh * 0.035);
     textAlign(CENTER,CENTER);
     for (let i = 0; i < 12; i++) {
         push();
         rotate((i * (360 / 12) - (minuteRotation - (360/60)*5)));
         if ((i + 1) * 5 != 60) {
-            text((i + 1) * 5, 0, -(height * 0.32));
+            text((i + 1) * 5, 0, -(minwh * 0.32));
         } else {
-            text(0, 0, -(height * 0.32));
+            text(0, 0, -(minwh * 0.32));
         }
         pop();
     }
 
     // Second numbers
-    textSize(height * 0.017);
+    textSize(minwh * 0.017);
     textAlign(CENTER,CENTER);
     for (let i = 0; i < 60; i++) {
         push();
         rotate(i * (360 / 60) - secondRotation);
-        text(i, 0, -(height * 0.275));
+        text(i, 0, -(minwh * 0.275));
         pop();
     }
 }

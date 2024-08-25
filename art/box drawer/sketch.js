1fa94e3bcb83bbb12c243e61ace51a830794689c
Eli <elipinals12@gmail.com>
2024-08-25 14:06:00 -0400

    Update sketch.js

--- art/box drawer/sketch.js
+++ art/box drawer/sketch.js
@@ -1,6 +1,8 @@
 var x, y;
 var fader = 115;
 var grow = 2 / 7;
+var before = true;
+
 function setup() {
     var cnv = createCanvas(window.innerWidth - 22, window.innerHeight - 22);
     var cnvx = (windowWidth - width) / 2;
@@ -8,14 +10,23 @@ function setup() {
     cnv.position(cnvx, cnvy);
 
     background(0);
+
+    fill(0, 255, 255);
 }
 
 function draw() {
-    x = mouseX;
-    y = mouseY;
-    noStroke();
-    fill(0, 255, 255, fader);
-    rect(x, y, grow * x, grow * y);
+    if (before) {
+        textSize(32);           // Set the text size
+        textAlign(CENTER, CENTER); // Align text to the center of the canvas
+        text('try to color in the screen', width / 2, height / 2);
+    }
+    else {
+        x = mouseX;
+        y = mouseY;
+        noStroke();
+        fill(0, 255, 255, fader);
+        rect(x, y, grow * x, grow * y);
+    }
 }
 
 function reset() {

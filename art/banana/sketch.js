10d70e22b432dde6ff77b25b6e42b3317f1e980f
elipinals12 <elipinals12@gmail.com>
2026-04-07 17:52:21 -0400

    update bananarave

--- art/banana/sketch.js
+++ art/banana/sketch.js
@@ -1,76 +1,85 @@
-// TODO
-
 var go = false;
-var cols = [];
-let img;
-var cnv, cnvx, cnvy;
+var cols = [255, 255, 255];
 var pause = false;
+var img;
+var backsound;
+var cnv, cnvx, cnvy;
+
+function preload() {
+  img = loadImage("banana.png");
+  backsound = loadSound("bananarave.mp3");
+}
 
 function setup() {
-    windowResized();
-
-    frameRate(20);
-
-    img = loadImage("banana.png");
-    backsound = loadSound("bananarave.mp3");
-    //backsound.setVolume(1);
+  windowResized();
+  frameRate(20);
 }
 
 function draw() {
-    background(255);
-    if (frameCount % 60 == 0 && !pause) {
-        frameRate(random(5, 25));
-    }
+  background(255);
 
-    if (go) {
-        if (!pause) {
-            cols[0] = random(0, 255);
-            cols[1] = random(0, 255);
-            cols[2] = random(0, 255);
-        }
-        background(cols[0], cols[1], cols[2]);
-    } else {
-        // if you need an offline (non banana) switch
-        // fill(0);
-        // rect(100, 100, 100, 100);
-    }
+  if (frameCount % 60 === 0 && !pause) {
+    frameRate(random(5, 25));
+  }
 
-    imageMode(CENTER);
-    image(img, width / 2, height / 2, width / 5, width / 5);
+  if (go && !pause) {
+    cols[0] = random(0, 255);
+    cols[1] = random(0, 255);
+    cols[2] = random(0, 255);
+  }
+
+  if (go) {
+    background(cols[0], cols[1], cols[2]);
+  }
+
+  imageMode(CENTER);
+  image(img, width / 2, height / 2, width / 5, width / 5);
 }
 
+function handleClick(x, y) {
+  // make sure coordinates are valid
+  x = constrain(floor(x), 0, width - 1);
+  y = constrain(floor(y), 0, height - 1);
+
+  var colAr = get(x, y);
+  var totalrgbnum = colAr[0] + colAr[1] + colAr[2] + colAr[3];
+
+  // white bg = 255*4 = 1020, anything less means we hit the banana
+  if (totalrgbnum < 1020) {
+    go = !go;
+    if (go) {
+      backsound.loop();
+    } else {
+      backsound.pause();
+    }
+  }
+}
+
+// desktop clicks
+function mousePressed() {
+  handleClick(mouseX, mouseY);
+}
+
+// mobile taps
 function touchStarted() {
-    // old way, just a circle, TERRIBLE
-    // if (abs(mouseX - width / 2) < 30 && abs(mouseY - height / 2) < 30) {
-    // new way, use pixel value, no need to make or use or think boundaries
-
-    let colAr = get(mouseX, mouseY);
-    let totalrgbnum = 0;
-    // print(colAr);
-    for (var rgbnum in colAr) {
-        print(rgbnum);
-        totalrgbnum += colAr[rgbnum];
-    }
-    // print(totalrgbnum);
-
-    if (totalrgbnum < 1020) {
-        go = !go;
-        if (go) {
-            backsound.loop();
-        } else {
-            backsound.pause();
-        }
-    }
-    totalrgbnum = 0;
+  if (touches.length > 0) {
+    handleClick(touches[0].x, touches[0].y);
+  } else {
+    handleClick(mouseX, mouseY);
+  }
+  // prevent default browser touch behavior (scrolling, zoom, etc.)
+  return false;
 }
 
 function keyPressed() {
-    if (keyCode == 32) pause = !pause;
+  if (keyCode === 32) {
+    pause = !pause;
+  }
 }
 
 function windowResized() {
-    cnv = createCanvas(windowWidth, windowHeight - .1);
-    cnvx = (windowWidth - width) / 2;
-    cnvy = (windowHeight - .1 - height) / 2;
-    cnv.position(cnvx, cnvy);
+  cnv = createCanvas(windowWidth, windowHeight - 0.1);
+  cnvx = (windowWidth - width) / 2;
+  cnvy = (windowHeight - 0.1 - height) / 2;
+  cnv.position(cnvx, cnvy);
 }
